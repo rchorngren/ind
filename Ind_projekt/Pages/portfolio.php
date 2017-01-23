8602af2bbc6149f93c74769ed88677bfd64ca5c3
Robert Horngren <robert.horngren@student.kyh.se>
2017-01-23 11:38:37 +0100

    Portfolio
    Portolio is now live, with two projects.
    SHOULD do with some styling though

--- Ind_projekt/Pages/portfolio.php
+++ Ind_projekt/Pages/portfolio.php
@@ -1,19 +1,16 @@
-﻿            <div id="mainLeft">
-                Född och uppvuxen i Stockholms skärgård. <br />
-                Började vid tidig ålder att hjälpa till med den fiskodling mina föräldrar drev (Vånö Fiskodling).<br />
-                Efter ett halvårs vikariat på Nämdö Förskola och en kortare anställning på McDonalds Sveavägen fick jag anställning på Dustin där jag blev kvar i närmare åtta år med diverse arbetsuppgifter.<br />
-                Gick därefter vidare till Atea som koordinator specialliserad på affärsområdet Mobilitet, något som jag är övertygad har en stor framtid inom IT-branschen.<br /><br />
-                Efter lite över fyra år bestämde jag mig dock för att byta inriktning från support och service, för att istället gå över mot utvecklingsidan. <br />
-                Tror att mina många års erfarenhet av just support och service kan komma till stor användning i min utbildning och senare fortsatta arbetsliv för att skapa användarvänliga och effektiva sidor och applikationer.<br /><br />
-                Min fritid tillbringar jag gärna utomhus, helst ute på Vånö. Fotografering tillhör också ett stort intresse (med en förkärlek för naturfoto), och på sommaren tar jag gärna fram vattenskidorna.
+﻿<div id="centerDiv">
+<table>
+    <tr>
+        <th>Individuellt projekt, realse ett</th>
+        <th>Första grupparbetet - Glada geten</th>
+    </tr>
+    <tr>
+       <th><a href="https://github.com/rchorngren/ind/commit/aa5cb7cd893548ce25434a634a4ac7cfd43b3441">Git it now!</th>
+       <th><a href="https://github.com/rchorngren/Glada-Geten">Git it now!</th>
+    </tr>
 
-            </div>
-            <div id="mainRight">
-                <b>Lite kort om mig;</b><br />
-                <ul>
-                    <li>Robert Horngren</li>
-                    <li>32 år</li>
-                    <li>Gymnasieutbildning från Nacka Gymnasium Elprogram med datorteknisk inriktning</li>
-                    <li>Naturfantast med förkärlek för paddling och fotografering</li>
-                </ul>
-            </div>
+</table>
+    </div>  
+
+
+</div>
