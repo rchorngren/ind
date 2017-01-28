30477b6f3e9126a1bd6218ac659405545b2d1cf0
Robert Horngren <robert.horngren@student.kyh.se>
2017-01-28 11:41:31 +0100

    Dynamic text
    The text displayed at home.php is now fetched from db
    ***
    Cosmetic fixes to portolio

--- Ind_projekt/Pages/home.php
+++ Ind_projekt/Pages/home.php
@@ -6,16 +6,18 @@ mysqli_query($db, "SET NAMES utf8");
 
 
 <div id="centerDiv"><br />
-    Välkommen till mitt första indivuiduella arbete på min väg till att bli fullfjädrad FrontEnd utvecklare. <br />
-    Tanken är att det ska komma lite mer information här som beskriver vem jag är, men först en liten reservation;<br /><br />
-    Då ett av kriterierna i projektet är att denna sida ska ligga live vid inlämningen av den så finns en viss risk
-    att eventuell framtida arbetsgivare råkar sen den. <br />
-    Till dig vill jag passa på att säga att jag är klart övertygad att denna sida inte representerar vad jag kommer vara kapabel till när jag väl kommer ut i arbetslivet på riktigt.
-    Sidan/projektet ska innehålla visst innehåll och funktionalitet och jag väljer därför att fokusera på detta och inte lägga tid på design och liknande.
-    <br />
-    Som en följd av detta ser sidan i nuvarande stund mindre vacker ut. <br /><br />
-    Jag hoppas därför att ingen tror att jag tycker "designen" på den här sidan är på något vis okej för att verkligen representera mig.<br />
-    Men alltid kul att veta att det finns mycket kvar att lära :)
+    <p>
+        <?php 
+            $query = "SELECT * FROM pages WHERE id=1";
+            $pages_result = mysqli_query($db, $query);
+            $page = mysqli_fetch_assoc($pages_result);
+        ?>
+    </p>
+    <p>
+        <?php 
+            echo $page['content']; 
+        ?>
+    </p>
 </div>
 
 <!--<div id="contactMe"></div>-->
--- Ind_projekt/Pages/portfolio.php
+++ Ind_projekt/Pages/portfolio.php
@@ -1,14 +1,14 @@
 ﻿<div id="centerDiv">
-<div id="portolioDiv">
-	<table>
-    	<tr>
-        	<th>Individuellt projekt, release ett</th>
-        	<th>Första grupparbetet - Glada geten</th>
-    	</tr>
-    	<tr>
-       		<th><a href="https://github.com/rchorngren/ind/commit/aa5cb7cd893548ce25434a634a4ac7cfd43b3441">Git it now!</a></th>
-       		<th><a href="https://github.com/rchorngren/Glada-Geten">Git it now!</a></th>
-    	</tr>
-	</table>
-</div>
+    <div id="portolio">
+        <div class="project">
+            <h4>Individuellt projekt, release ett</h4>
+            <p><a href="https://github.com/rchorngren/ind/commit/aa5cb7cd893548ce25434a634a4ac7cfd43b3441">Git it now!</a></p>
+            <hr>
+        </div>
+        <div class="project">
+            <h4>Första grupparbetet - Glada geten</h4>
+            <p><a href="https://github.com/rchorngren/Glada-Geten">Git it now!</a></p>
+            <hr>
+        </div>
+    </div>
 </div>
